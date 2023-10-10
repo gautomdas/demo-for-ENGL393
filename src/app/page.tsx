2e69405d7b34f03f9fff2c2b5bb6e5021a2c58d9
Gautom Das <gautomdas4@gmail.com>
2023-10-10 00:35:18 -0400

    Fixed linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Hi my name is Gautom Das, I'm a student at the University of Maryland
+        Hi my name is Gautom Das, Im a student at the University of Maryland
       </div>
     </main>
   );
